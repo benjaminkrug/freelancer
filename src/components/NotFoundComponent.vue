4c1d9679c45c516da6d589c6022264c29815b768
benni <krug.benjamin.92>
2020-12-12 18:16:57 +0100

    init

---
+++ src/components/NotFoundComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    not found
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+  },
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import Router from "vue-router";
-import { publicPath } from '../../vue.config.js'
+//import { publicPath } from '../../vue.config.js'
 
 //import Home from '../components/Home.vue'
 
 Vue.use(Router);
 
 const router = new Router({
-  mode: "history",
-  base: publicPath,
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '', component: () => import('../components/HomeBody.vue')
@@ -18,7 +18,8 @@ const router = new Router({
     },
     {
       path: '/projects', component: () => import('../components/Projects.vue')
-    }
+    },
+    { path: '*', component:  () => import('../components/NotFoundComponent.vue') }
   ]
 });
 
