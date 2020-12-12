1a1370c66f4b58c3f0a9e186b5a6b7f2ec66ebae
benni <krug.benjamin.92>
2020-12-12 17:40:50 +0100

    init

--- vue.config.js.js
+++ vue.config.js.js
@@ -1,4 +1,4 @@
 module.exports = {
   /* ... */
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/fervent-snyder-5c85fd.netlify.app/' : '/'
 }
