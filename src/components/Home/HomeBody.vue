347754616f17f192d69c1ebd2e0cbee649d569c1
benni <krug.benjamin.92>
2020-12-27 18:52:27 +0100

    save

--- src/components/Home/HomeBody.vue
+++ src/components/Home/HomeBody.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="HomeBody" id="homeBody" @wheel="handleScroll">
-    <div id="circlebox">
+    <div v-for="box in boxes" :key="box.id" class="circlebox" :id="box.id">
     </div>
   </div>
 </template>
@@ -13,68 +13,63 @@ export default {
   },
   data () {
     return {
-      box:[{
+      boxes:[{
         style: {
           'background-color': 'red'
         },
-        id: 1
+        id: 'circlebox1'
       }],
-      boxHeight: 100,
-      boxWidth: 100,
-      boxBottom: 0,
-      boxLeft: 0,
-      directionRight: true,
       position: 0,
+      rows: 128,
+      columns: 32,
     }
   },
   computed: {
-    circlebox() {
-      return {
-        style: {
-          'transition-timing-function': 'linear',
-          'transition': 'transform 2s',
-          'position': 'absolute',
-          'bottom': this.boxBottom + 'px',
-          'left': this.boxLeft + 'px',
-          'background-color': 'red',
-          'height':this.boxHeight + 'px',
-          'width': this.boxWidth + 'px',
-        }
-      }
-    },
     HomeBodyElement() {
       return document.getElementById('homeBody');
     },
     CircleboxElement() {
-      return document.getElementById('circlebox');
-    }
+      return document.getElementById('circlebox1');
+    },
+
   },
   methods: {
     async handleScroll (value) {
       var next = value.deltaY > 0 ? -1 : +1;
       this.position += next;
 
-      var rows = 128; // muss
-      var columns = 32;
-
       if(this.position < 0 ){
         this.position = 0
       }
-      if(this.position > rows ){
-        this.position = rows
+      if(this.position > this.rows ){
+        this.position = this.rows
       }
       var clientHeight = this.HomeBodyElement.clientHeight - this.CircleboxElement.clientHeight;
-      var boxBottom = (-1) * this.position * clientHeight / rows;
+      var boxBottom = (-1) * this.position * clientHeight / this.rows;
       var clientWidth = this.HomeBodyElement.clientWidth - this.CircleboxElement.clientWidth;
 
-      var rest = parseInt(this.position % (columns * 2)); //gibt 4 elemente links->mitte->rechts->mitte
-      var referenzAbweichung = Math.abs(rest - columns); //rechts sind alle die die 2 als rest haben ( +- 1 sind die mitte/ +-2 sind links)
-      var offsetIndex = Math.abs(referenzAbweichung- columns); // hier wird alles umgedreht links ist jetzt 0 abs(2-2) mitte ist 1 abs(1-2) rechts ist abs(0-2)
-      var boxLeft = offsetIndex * clientWidth / columns;
-      console.log(offsetIndex);
+      var rest = parseInt(this.position % (this.columns * 2)); //gibt 4 elemente links->mitte->rechts->mitte
+      var referenzAbweichung = Math.abs(rest - this.columns); //rechts sind alle die die 2 als rest haben ( +- 1 sind die mitte/ +-2 sind links)
+      var offsetIndex = Math.abs(referenzAbweichung- this.columns); // hier wird alles umgedreht links ist jetzt 0 abs(2-2) mitte ist 1 abs(1-2) rechts ist abs(0-2)
+      var boxLeft = offsetIndex * clientWidth / this.columns;
       this.CircleboxElement.style.transform = 'translateY('+ boxBottom +'px)';
       this.CircleboxElement.style.transform += 'translateX('+ boxLeft +'px)';
 
+    }
+  },
+  watch: {
+    position (newVal) { // watch it
+      console.log('Prop changed: ', newVal)
+
+      if(newVal / this.columns > this.boxes.length-1)
+      {
+        this.boxes.push({
+          style: {
+            'background-color': 'blue'
+          },
+          id: 'circlebox' + this.boxes.length + 1
+        })
+      }
     }
   },
   created() {
@@ -90,7 +85,7 @@ export default {
   position: relative;
   margin: auto;
 }
-#circlebox{
+.circlebox{
   transition: transform 1.5s;
   position: absolute;
   bottom: 0;
